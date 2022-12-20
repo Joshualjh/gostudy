ea2bc4c24e12f412d31037d74c62b20c7c3df691
Joshualjh <aak1009@naver.com>
2022-12-20 15:03:48 +0900

    16

--- comento/2weeks2/main.go
+++ comento/2weeks2/main.go
@@ -8,6 +8,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Person struct {
+	Name    string `form:"name" json:"name"`
+	Address string `form:"address" json:"address"`
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -20,13 +25,22 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-
 			return
 		}
+
 		log.Println(file.Filename)
 		c.SaveUploadedFile(file, file.Filename)
 		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
 
+	})
+
+	r.GET("/test", func(c *gin.Context) {
+		person := Person{}
+		if err := c.ShouldBind(&person); err != nil {
+			fmt.Println("Bind Err")
+		}
+
+		c.JSON(200, person)
 	})
 
 	r.Run()
